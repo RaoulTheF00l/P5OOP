4bdc36a1893333e3eff22133d7b41594f7362dfb
RaoulTheF00l <conore21@gmail.com>
2026-05-27 10:20:50 -0400

    Added player_stats db. Need to change extends to node.

---
+++ persona-style-sandbox/Scripts/Player_stats.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+
+#Add social stats below
+@export var kindness: int = 0
+@export var proficiency: int = 0
+@export var guts: int = 0
+@export var charm: int = 0
+@export var yen: int = 0
+
+#Add combat stats below
