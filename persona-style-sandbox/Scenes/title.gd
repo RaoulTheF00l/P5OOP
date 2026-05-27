9f1380213d46051ee38d65b63da76505d5a91c34
RaoulTheF00l <conore21@gmail.com>
2026-05-27 09:24:34 -0400

    Added Title Screen + change to gamehub button functionality

---
+++ persona-style-sandbox/Scenes/title.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+@onready var play_button: Button = $Layout/PlayButton
+@onready var quit_buttom: Button = $Layout/QuitButtom
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	play_button.pressed.connect(_on_play_button_pressed)
+	
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	pass
