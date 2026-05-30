7514c0c4f9dc55da9d7d93a827dc1d39557fbf9c
RaoulTheF00l <conore21@gmail.com>
2026-05-30 12:56:24 -0400

    Began npc script

--- persona-style-sandbox/Scripts/npc.gd
+++ persona-style-sandbox/Scripts/npc.gd
@@ -1,1 +1,16 @@
 extends StaticBody2D
+
+
+enum NPCtype { NPC, ENEMY, FRIENDLY_SHADOW }
+
+@export var npcName : String = "Jack Frost"
+@export var npcType : NPCtype = NPCtype.ENEMY
+@export var dialouge: array[String] = ["Hee-ho-he!"]
+
+var dialougeIndex : int = 0
+
+func interact() -> void:
+	pass
+
+func showDialouge() -> void:
+	pass
