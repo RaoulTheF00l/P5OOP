12d1ccbfeb16fbb147d87582a3002ef77387df80
RaoulTheF00l <conore21@gmail.com>
2026-05-30 15:33:36 -0400

    Finished Combat loop. Known crash: on win battle rewards not implemented

--- persona-style-sandbox/Scripts/combat.gd
+++ persona-style-sandbox/Scripts/combat.gd
@@ -11,6 +11,16 @@ var enemy_max_hp: int = 60
 var enemy_attack: int = 12
 var enemy_def: int = 8
 
+var exp_reward: int = 30
+var yen_reward: int = 200
+
+
+@onready var enemyname: Label = $EnemyArea/EnemyName
+@onready var enemyhp: Label = $EnemyArea/EnemyHP
+@onready var player_name: Label = $PartyInfo/PlayerName
+@onready var hp_label: Label = $PartyInfo/HPLabel
+@onready var sp_plabel: Label = $PartyInfo/SPlabel
+
 
 @onready var attack_button: Button = $ActionMenu/AttackButton
 @onready var skill_button: Button = $ActionMenu/SkillButton
@@ -31,11 +41,11 @@ func combat_handler() -> void:
 		set_buttons_enabled(false)
 
 func update_ui() -> void:
-	$EnemyArea/EnemyName.text = enemy_name
-	$EnemyArea/EnemyHP.text = "Hp: " + str(enemy_hp) + "/" + str(enemy_max_hp)
-	$PlayerName.text = "Ren"
-	$HPLabel.text = "HP: " + str(PlayerStats.hp) + "/" + str(PlayerStats.max_hp)
-	$SPLabel.text = "SP: " + str(PlayerStats.sp) + "/" + str(PlayerStats.max_sp)
+	enemyname.text = enemy_name
+	enemyhp.text = "Hp: " + str(enemy_hp) + "/" + str(enemy_max_hp)
+	player_name.text = "Ren"
+	hp_label.text = "HP: " + str(PlayerStats.hp) + "/" + str(PlayerStats.max_hp)
+	sp_plabel.text = "SP: " + str(PlayerStats.sp) + "/" + str(PlayerStats.max_sp)
 	$CombatLog.text = "Current State: " + str(state)
 
 func set_buttons_enabled(enabled: bool) -> void:
@@ -47,30 +57,43 @@ func set_buttons_enabled(enabled: bool) -> void:
 
 func _on_attack() -> void:
 	state = CombatState.PLAYER_ATTACK
-	var damage = PlayerStats.atk - enemy_def
+	var damage = max(1, PlayerStats.atk - enemy_def)
 	enemy_hp -= damage
 	check_enemy_defeated()
-	if state != CombatState.WON:
+	if state == CombatState.WON:
+		return  # stop here, go_to_rewards() is handling it
 		_on_enemy_turn()
-	update_ui()
+		update_ui()
+		combat_handler()
+
 
 func _on_skill() -> void:
 	state = CombatState.PLAYER_MATK
-	var damage = PlayerStats.matk - enemy_def
+	var damage = max(1, PlayerStats.matk - enemy_def)
 	enemy_hp -= damage
 	PlayerStats.sp -= 10
 	check_enemy_defeated()
+	if state == CombatState.WON:
+		return
+	_on_enemy_turn()
 	update_ui()
+	combat_handler()
+
 
 func check_enemy_defeated() -> void:
 	if enemy_hp <= 0:
 		state = CombatState.WON
 		update_ui()
+		await get_tree().create_timer(0.5).timeout
+		go_to_rewards()
 
 func check_player_defeated() -> void:
 	if PlayerStats.hp <= 0:
 		state = CombatState.LOST
 		update_ui()
+		await get_tree().create_timer(0.5).timeout
+		get_tree().change_scene_to_file("res://Scenes/leblanc.tscn")
+
 
 func reset() -> void:
 	state = CombatState.PLAYER_TURN
@@ -78,18 +101,22 @@ func reset() -> void:
 	PlayerStats.sp = PlayerStats.max_sp
 	update_ui()
 
+
 func _on_attack_button_pressed() -> void:
 	_on_attack()
+
 
 func _on_skill_button_pressed() -> void:
 	_on_skill()
 
+
 func _on_enemy_turn() -> void:
 	_on_enemy_attack()
 
+
 func _on_enemy_attack() -> void:
 	state = CombatState.ENEMY_ATTACK
-	var damage = enemy_attack - PlayerStats.def
+	var damage = max(1, enemy_attack - PlayerStats.def)
 	PlayerStats.hp -= damage
 	check_player_defeated()
 	if state != CombatState.LOST:
@@ -98,3 +125,10 @@ func _on_enemy_attack() -> void:
 	update_ui()
 
 #to add: On_scene_change. On_battle_rewards_scene, return to debug menu button
+
+func go_to_rewards() -> void:
+	var rewards_scene = load("res://Scenes/battle_rewards.tscn")
+	var rewards = rewards_scene.instantiate()
+	rewards.setup(exp_reward, yen_reward)
+	get_tree().root.add_child(rewards)
+	queue_free()
