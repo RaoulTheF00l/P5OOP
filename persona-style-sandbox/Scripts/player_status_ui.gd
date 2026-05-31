f9edcb7fe776361aee3c597f39f0ef275ee02224
RaoulTheF00l <conore21@gmail.com>
2026-05-31 15:20:40 -0400

    Write Player Staus UI script. Not implemented

---
+++ persona-style-sandbox/Scripts/player_status_ui.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var yen_label: Label = $Panel/VBoxContainer/YenLabel
+@onready var exp_label: Label = $Panel/VBoxContainer/ExpLabel
+
+var last_yen: int = -1
+var last_exp: int = -1
+
+
+func _ready() -> void:
+	refresh()
+
+
+func _process(_delta: float) -> void:
+	if PlayerStats.yen != last_yen or PlayerStats.exp != last_exp:
+		refresh()
+
+
+func refresh() -> void:
+	last_yen = PlayerStats.yen
+	last_exp = PlayerStats.exp
+	
+	yen_label.text = "Yen: " + str(PlayerStats.yen)
+	exp_label.text = "Total EXP: " + str(PlayerStats.exp)
