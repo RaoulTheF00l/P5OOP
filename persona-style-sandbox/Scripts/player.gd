09745b7f921bb78e32a818706ae9a29728b60720
RaoulTheF00l <conore21@gmail.com>
2026-05-31 11:04:31 -0400

    Working out interaction bugs

--- persona-style-sandbox/Scripts/player.gd
+++ persona-style-sandbox/Scripts/player.gd
@@ -5,20 +5,29 @@ const SPEED = 100
 var nearby_npc = null
 
 func _ready():
-	$InteractZone.body_entered.connect(_on_interact_zone_entered)
-	$InteractZone.body_exited.connect(_on_interact_zone_exited)
+	pass
 
 func _physics_process(_delta):
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
 	velocity = direction * SPEED
 	move_and_slide()
 
+
+
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Interact") and nearby_npc != null:
-		nearby_npc.interact()
+	if event is InputEventKey and not event.echo and event.pressed:
+		if event.physical_keycode == KEY_Z:
+			if nearby_npc != null:\
+			if nearby_npc != null and nearby_npc.has_method("interact"):
+					nearby_npc.interact()
 
-func _on_interact_zone_entered(area: Node2D) -> void:
-	nearby_npc = area.get_parent()
 
-func _on_interact_zone_exited(area: Node2D) -> void:
+func _on_interact_zone_body_entered(body: Node2D) -> void:
+	nearby_npc = body.get_parent()
+
+
+
+
+
+func _on_interact_zone_body_exited(_body: Node2D) -> void:
 	nearby_npc = null
