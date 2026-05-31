ef84a0d8cf0a226b909526dfff0b9728d8c3513e
RaoulTheF00l <conore21@gmail.com>
2026-05-31 13:42:54 -0400

    Checkpoint before adding dialogue box

--- persona-style-sandbox/Scripts/npc.gd
+++ persona-style-sandbox/Scripts/npc.gd
@@ -5,9 +5,9 @@ enum NPCType { NPC, ENEMY, FRIENDLY_SHADOW }
 
 @export var npc_name : String = "Jack Frost"
 @export var npc_type : NPCType = NPCType.ENEMY
-@export var dialouge: Array[String] = ["Hee-ho-he!"]
+@export var dialogue: Array[String] = ["Hee-ho-he!"]
 
-var dialouge_index : int = 0
+var dialogue_index : int = 0
 
 func interact() -> void:
 	match npc_type:
@@ -21,10 +21,10 @@ func interact() -> void:
 			get_tree().change_scene_to_file("res://Scenes/combat.tscn")
 
 func show_dialouge() -> void:
-	if dialouge_index < dialouge.size():
-		print(npc_name + ": " + dialouge[dialouge_index])
-		dialouge_index += 1
+	if dialogue_index < dialogue.size():
+		print(npc_name + ": " + dialogue[dialogue_index])
+		dialogue_index += 1
 	else:
-		dialouge_index = 0
-		print(npc_name + ": " + dialouge[dialouge_index])
+		dialogue_index = 0
+		print(npc_name + ": " + dialogue[dialogue_index])
 	
